8bb85d3736e3172aef1a273994ba6ecf74336086
Lucas Delobelle <lucas@ld-web.net>
2019-11-18 15:13:45 +0100

    App => Class Component, API call in componentDidMount

--- src/App.js
+++ src/App.js
@@ -1,27 +1,51 @@
-import React from "react";
+import React, { Component } from "react";
 import Nav from "./Nav";
 import Axios from "axios";
 
-const App = () => {
-  let users = [];
+class App extends Component {
+  constructor(props) {
+    super(props);
+    console.log("Construction du composant");
+    // Le constructeur est le seul endroit où on peut manipuler directement le state
+    this.state = {
+      users: []
+    };
+  }
 
-  Axios.get("https://jsonplaceholder.typicode.com/users")
-    .then(res => {
-      console.log("Première promesse avant affectation users", users);
-      users = res.data;
-    })
-    .then(() => {
-      console.log("Deuxième promesse", users);
-    });
+  componentDidMount = () => {
+    console.log("Component did mount : appel API")
+    Axios.get("https://jsonplaceholder.typicode.com/users").then(res =>
+      // Dans toutes les autres méthodes, on utilisera this.setState
+      this.setState({
+        users: res.data
+      })
+    );
+  };
 
-  return (
-    <>
-      <Nav />
-      <div className="container-fluid">
-        <h1>Utilisateur</h1>
-      </div>
-    </>
-  );
-};
+  render = () => {
+    const { users } = this.state;
+
+    console.log("Dans le render avant return");
+    return (
+      <>
+        <Nav />
+        <div className="container">
+          <div className="row">
+            <div className="col p-2">
+              <h1>Utilisateurs</h1>
+            </div>
+          </div>
+          <div className="row">
+            {users.map(user => (
+              <div className="col-12 col-lg-4" key={user.id}>
+                {user.name}
+              </div>
+            ))}
+          </div>
+        </div>
+      </>
+    );
+  };
+}
 
 export default App;
