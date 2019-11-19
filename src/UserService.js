679c9274c76641aff3a32e543e6b4ec12aafde47
Lucas Delobelle <lucas@ld-web.net>
2019-11-19 15:42:47 +0100

    Constants definition + update UserService

--- src/UserService.js
+++ src/UserService.js
@@ -1,5 +1,11 @@
 import Axios from "axios";
+import { USER_ENDPOINT } from "./constants";
 
 export const getUsers = () => (
-  Axios.get("https://jsonplaceholder.typicode.com/users").then(res => res.data)
+  Axios.get(USER_ENDPOINT).then(res => res.data)
 );
+
+export const getUser = async id => {
+  const res = await Axios.get(`${USER_ENDPOINT}/${id}`);
+  return res.data;
+}
